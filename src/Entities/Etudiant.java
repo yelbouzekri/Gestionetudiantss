ea331c89dda755111e44676fbbeb8a1f379edbdc
younes <younes@192.168.8.121>
2020-01-23 14:19:53 +0000

    bjr

---
+++ src/Entities/Etudiant.java
@@ -0,0 +1,21 @@
+package Entities;
+
+import java.text.DecimalFormat;
+
+public class Etudiant {
+
+	public int cne;
+	public String nom;
+	public String prenom;
+public 	double note;
+public 	static double notemoyenne;
+
+
+public void imprimer()
+{
+	System.out.println("Bonjour tout le monde");
+	}
+public void imprimer(String f)
+{System.out.println("bonsour Mr....." +f);
+	}
+}
